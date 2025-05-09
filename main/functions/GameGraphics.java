1a7d3670a81e5952a19e2da5f979ef7a97e8ef0d
JustinRomanelli <justin117247@student-schoolsofwestfield.org>
2025-05-09 14:44:55 +0000

    Added the ability to change the color of a button, will need to be tested.

--- main/functions/GameGraphics.java
+++ main/functions/GameGraphics.java
@@ -18,6 +18,8 @@ public class GameGraphics {
     // The frame that contains the UI.
     private static JFrame frame;
 
+    private static JButton[] allButtons;
+
     // All of the image paths
     // NOTE: Only DIE_ONE_PATH currently has an image!
     private static final String DIE_ONE_PATH = "./imgs/die_1.png";
@@ -33,6 +35,8 @@ public class GameGraphics {
      */
     public static void show() {
         frame = new JFrame();
+
+        allButtons = new JButton[5];
 
         frame.setSize(1600, 900);
 
@@ -71,10 +75,31 @@ public class GameGraphics {
         JButton button = new JButton("Hold");
 
         // The String in the first parameter of HoldButtonAction is what is printed when the button is pressed.
-        button.addActionListener(new HoldButtonAction("Hello World! TEST", new ImageIcon(DIE_ONE_PATH), btnIndex));
+        button.addActionListener(new HoldButtonAction("Hello World! TEST", new ImageIcon(DIE_ONE_PATH)));
 
         button.setBounds(x, y, 100, 20);
 
+        allButtons[btnIndex] = button;
+
         frame.add(button);
+    }
+
+    /*
+     * Changes the color of a hold button, such that it shows feedback for whether a die is held or not.
+     * Parameters: Whether the button is to be enabled (now holding) or not, the index of the button (0-4).
+     */
+    private static void colorHoldButton(boolean colorEnabled, int btnIndex) {
+        // TODO: Work on this function and ensure that it works with a GUI!
+        JButton button = allButtons[btnIndex];
+
+        // ? Not sure if this works...
+        assert button != null;
+
+        if (colorEnabled) {
+            button.setBackground(Color.green);
+        }
+        else {
+            button.setBackground(Color.gray);
+        }
     }
 }
