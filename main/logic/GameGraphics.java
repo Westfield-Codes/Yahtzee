71c87aad041fff0b949ea6757d3adbfaf4f5b0b7
JustinRomanelli <justin117247@student-schoolsofwestfield.org>
2025-05-13 08:22:25 -0400

    Added comments to GameGraphics.java

--- main/logic/GameGraphics.java
+++ main/logic/GameGraphics.java
@@ -18,10 +18,6 @@ public class GameGraphics {
     // The frame that contains the UI.
     private static JFrame frame;
 
-    private static JButton[] allButtons;
-
-    public static Die[] allDice;
-
     // All of the image paths
     // NOTE: Only DIE_ONE_PATH currently has an image!
     private static final String DIE_ONE_PATH = "./imgs/die_1.png";
@@ -36,38 +32,44 @@ public class GameGraphics {
      * Currently a work in progress. 
      */
     public static void show() {
+
+        // Sets up the JFrame (window that holds the graphics)
         frame = new JFrame();
-
-        allButtons = new JButton[5];
-
-        allDice = new Die[5];
 
         frame.setSize(1600, 900);
 
+        // This allows us to place elements absolutely, so that it does not default to a grid-like shape.
+        // May be changed in the future, but for early in the project, this works.
         frame.setLayout(null);
 
-        GameGraphics.drawDie(DIE_ONE_PATH, 0, 275, 150);
-        GameGraphics.drawDie(DIE_ONE_PATH, 1, 500, 150);
-        GameGraphics.drawDie(DIE_ONE_PATH, 2, 725, 150);
-        GameGraphics.drawDie(DIE_ONE_PATH, 3, 950, 150);
-        GameGraphics.drawDie(DIE_ONE_PATH, 4, 1175, 150);
+        // Draws the dice, with the formate IMG_FILE_PATH, x position, y position
+        GameGraphics.drawDie(DIE_ONE_PATH, 275, 150);
+        GameGraphics.drawDie(DIE_ONE_PATH, 500, 150);
+        GameGraphics.drawDie(DIE_ONE_PATH, 725, 150);
+        GameGraphics.drawDie(DIE_ONE_PATH, 950, 150);
+        GameGraphics.drawDie(DIE_ONE_PATH, 1175, 150);
 
+        // Draws the hold buttons under the dice, with the x and y coordinates, then the button index.
+        // The button index is currently used to identify which button is pressed, through a JButton[] array.
         GameGraphics.drawHoldButton(300, 350, 0);
         GameGraphics.drawHoldButton(525, 350, 1);
         GameGraphics.drawHoldButton(750, 350, 2);
         GameGraphics.drawHoldButton(975, 350, 3);
         GameGraphics.drawHoldButton(1200, 350, 4);
 
+        // Informally creates the frame, so that the user can see it.
         frame.setVisible(true);
     }
 
     /*
      * Draws the die to the screen.
+     * Parameters: path is the path to the image to draw,
+     *             x is the x coordinate to draw, from the top left corner,
+     *             y is the y coordinate to draw, from the top-left corner.
+     * Returns: None
      */
-    public static void drawDie(String path, int index, int x, int y) {
-        Die img = new Die(path, index);
-
-        allDice[index] = img;
+    public static void drawDie(String path, int x, int y) {
+        Die img = new Die(path);
 
         JLabel imgLabel = new JLabel(img);
 
@@ -77,37 +79,21 @@ public class GameGraphics {
         frame.add(imgLabel);
     }
 
+    /*
+     * Draws the "Hold" Button to the screen, currently under the die.
+     * Parameters: x is the x coordinate to draw, from the top left corner,
+     *             y is the y coordinate to draw, from the top-left corner.
+     *             btnIndex is a number [0-4] (inclusive) that allows the program to determine what button is being used.
+     * Returns: None
+     */
     private static void drawHoldButton(int x, int y, int btnIndex) {
         JButton button = new JButton("Hold");
 
         // The String in the first parameter of HoldButtonAction is what is printed when the button is pressed.
-        button.addActionListener(new HoldButtonAction(btnIndex));
+        button.addActionListener(new HoldButtonAction("Hello World! TEST", new ImageIcon(DIE_ONE_PATH), btnIndex));
 
         button.setBounds(x, y, 100, 20);
 
-        button.setBackground(Color.gray);
-
-        allButtons[btnIndex] = button;
-
         frame.add(button);
-    }
-
-    /*
-     * Changes the color of a hold button, such that it shows feedback for whether a die is held or not.
-     * Parameters: Whether the button is to be enabled (now holding) or not, the index of the button (0-4).
-     */
-    public static void colorHoldButton(boolean colorEnabled, int btnIndex) {
-        // TODO: Work on this function and ensure that it works with a GUI!
-        JButton button = allButtons[btnIndex];
-
-        // ? Not sure if this works...
-        assert button != null;
-
-        if (colorEnabled) {
-            button.setBackground(Color.green);
-        }
-        else {
-            button.setBackground(Color.gray);
-        }
     }
 }
