8802ff3c65c26c0f8b6d56e5e9794f087250d1e1
sophie202394 <sophie202394@student-schoolsofwestfield.org>
2025-05-23 08:18:02 -0400

    Create DiceCup.java

---
+++ main/logic/DiceCup.java
@@ -0,0 +1,28 @@
+package main.functions;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class DiceCup {
+
+String name = "";
+private int[] hand;
+private boolean[] hold;
+
+public DiceCup(String name) {
+    this.name = name;
+    hand = new int[] {0, 0, 0, 0, 0};
+    hold = new boolean[] {false, false, false, false, false};
+}
+
+getHand() {
+    return this.hand;
+}
+
+public boolean getHeld(index){
+
+
+}
+
+   
+}
