ffa561d0a498120b3af01408ee12c0e873004c0e
BrandonGuyott <brandon100743@student-schoolsofwestfield.org>
2025-05-23 08:19:37 -0400

    Add files via upload
    Psuedocode/comment form of Round class

---
+++ main/logic/Round.java
@@ -0,0 +1,41 @@
+public class Round {
+
+
+
+    // STATIC METHODS
+    
+    // May roll dice up to 3 times, may stop & score after 1st or 2nd roll
+    // To roll dice, place them in dice cup & roll
+
+
+
+
+
+    // First roll: all 5 dice
+    // Set keepers aside
+    // score now? or roll again?
+
+
+
+
+    // Reroll any/all dice you want
+    // no need to declae combination to roll for.
+
+
+
+
+
+    // Third (final roll): reroll any dice you want
+    // After, must fill in a box on score card with a score or zero
+
+
+
+
+
+
+
+
+
+
+
+}
