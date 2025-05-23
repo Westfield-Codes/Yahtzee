1ed99a3a839efa8f9bb8879ee15a3d5a646b3ae2
PashaF1 <pavlo207407@student-schoolsofwestfield.org>
2025-05-23 08:22:14 -0400

    Add files via upload

--- main/ui/event/HoldButtonAction.java
+++ main/ui/event/HoldButtonAction.java
@@ -1,22 +1,26 @@
-package main.functions.event;
+package main.ui.event;
 
 import javax.swing.*;
 import javax.swing.event.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import main.functions.GameLogic;
-import main.functions.GameGraphics;
+import main.logic.GameLogic;
+import main.logic.GameGraphics;
 
 public class HoldButtonAction implements ActionListener { // extends AbstractAction {
     
     private int btnIndex;
+    private String text;
+    private ImageIcon img;
 
-    public HoldButtonAction(int index) {
+    public HoldButtonAction(String text, ImageIcon img, int index) {
+        this.text = text;
+        this.img = img;
         this.btnIndex = index;
     }
 
     public void actionPerformed(ActionEvent e) {
-        GameLogic.holdDie(GameGraphics.allDice[this.btnIndex]);
+        //GameLogic.holdDie(GameGraphics.allDice[this.btnIndex]);
     }
 }
