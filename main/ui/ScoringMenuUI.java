d4e75744db40068a70ecfbb72190892ba5c4c8ad
JustinRomanelli <justin117247@student-schoolsofwestfield.org>
2025-05-25 21:40:47 -0400

    Updated the UI to make the score buttons disappear when pressed, replacing it with a placeholder 0.

--- main/ui/ScoringMenuUI.java
+++ main/ui/ScoringMenuUI.java
@@ -11,14 +11,21 @@ import main.ui.GameGraphics;
 import main.ui.event.ScoreButtonAction;
 
 public class ScoringMenuUI {
-  private static JPanel container;
+  public static JPanel container;
 
   public static JButton[] btnsScore;
 
-  private static String[] labelIDs = {
+  public static JLabel[] jLabels;
+
+  public static String[] labelIDs = {
       "Ones", "Twos", "Threes", "Fours", "Fives", "Sixes",
       "Three of a Kind", "Four of a Kind", "Full House", "Small Straight",
       "Large Straight", "Yahtzee", "Chance"
+  };
+
+  public static String[] buttonIDs = {
+      "Score", "Score", "Score", "Score", "Score", "Score",
+      "Score", "Score", "Score", "Score", "Score", "Score", "Score",
   };
 
   public static void createScoringMenu() {
@@ -27,20 +34,18 @@ public class ScoringMenuUI {
     // container.setBounds(100, 100, 250, 500);
 
     // NOTE: Actual bounds are below. The ones above are for TESTING ONLY.
-    container.setBounds(1300, 100, 250, 500);
-
-    // container.setBackground(Color.BLUE);
+    container.setBounds(1300, 100, 250, 496);
 
     // Creates a temporary border around the scoreboard.
     Border b = BorderFactory.createLineBorder(Color.BLACK);
 
-    GridLayout layout = new GridLayout(13, 2, 10, 10);
+    GridLayout layout = new GridLayout(13, 2);
 
     container.setLayout(layout);
 
     btnsScore = new JButton[13];
 
-    JLabel[] jLabels = new JLabel[13];
+    jLabels = new JLabel[13];
 
     for (int i = 0; i < 13; i++) {
       jLabels[i] = new JLabel(labelIDs[i]);
@@ -53,7 +58,7 @@ public class ScoringMenuUI {
       btnsScore[i].setHorizontalAlignment(JLabel.CENTER);
 
       // Uncomment the line below to disable the button when it is pressed.
-      // btnsScore[i].addActionListener(new ScoreButtonAction(i));
+      btnsScore[i].addActionListener(new ScoreButtonAction(i));
 
       container.add(jLabels[i]);
       container.add(btnsScore[i]);
@@ -63,4 +68,5 @@ public class ScoringMenuUI {
 
     GameGraphics.frame.add(container);
   }
+
 }
--- main/ui/event/ScoreButtonAction.java
+++ main/ui/event/ScoreButtonAction.java
@@ -2,8 +2,10 @@ package main.ui.event;
 
 import javax.swing.*;
 import javax.swing.event.*;
+import javax.swing.border.Border;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Color;
 
 import main.ui.GameGraphics;
 import main.ui.ScoringMenuUI;
@@ -17,8 +19,49 @@ public class ScoreButtonAction implements ActionListener {
   }
 
   public void actionPerformed(ActionEvent e) {
-    JButton scoreButton = ScoringMenuUI.btnsScore[btnIndex];
+    ScoringMenuUI.buttonIDs[btnIndex] = "";
+  
+    ScoringMenuUI.container.removeAll();
 
-    scoreButton.setEnabled(false);
+    rebuildContainer(ScoringMenuUI.container, ScoringMenuUI.labelIDs, ScoringMenuUI.buttonIDs);
+
+    ScoringMenuUI.container.revalidate();
+
+    ScoringMenuUI.container.repaint();
+  }
+
+  private void rebuildContainer(JPanel container, String[] labels, String[] buttons) {
+    Border border = BorderFactory.createLineBorder(Color.BLACK);
+
+    for (int i = 0; i < labels.length; i++) {
+      JLabel label = new JLabel(labels[i]);
+
+      label.setBorder(border);
+
+      label.setHorizontalAlignment(JLabel.CENTER);
+
+      container.add(label);
+
+      if (ScoringMenuUI.buttonIDs[i].equals("")) {
+        label = new JLabel("0");
+
+        label.setBorder(border);
+
+        label.setHorizontalAlignment(JLabel.CENTER);
+
+        container.add(label);
+      }
+      else {
+        JButton button = new JButton(buttons[i]);
+
+        button.setBorder(border);
+
+        button.setHorizontalAlignment(JLabel.CENTER);
+
+        button.addActionListener(new ScoreButtonAction(i));
+
+        container.add(button);
+      }
+    }
   }
 }
