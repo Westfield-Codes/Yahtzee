f6ae050044ee780f6d3e7f385694205c5111d8b8
sophie202394 <sophie202394@student-schoolsofwestfield.org>
2025-05-28 07:34:29 -0400

    Update DiceCup.java

--- main/logic/DiceCup.java
+++ main/logic/DiceCup.java
@@ -1,16 +1,14 @@
 package main.functions;
 
 import java.sql.Array;
-import java.util.ArrayList;
 
 public class DiceCup {
 
-String name = "";
+
 private int[] hand;
 private boolean[] hold;
 
-public DiceCup(String name) {
-    this.name = name;
+public DiceCup() {
     hand = new int[] {0, 0, 0, 0, 0};
     hold = new boolean[] {false, false, false, false, false};
 }
@@ -18,11 +16,4 @@ public DiceCup(String name) {
 getHand() {
     return this.hand;
 }
-
-public boolean getHeld(index){
-
-
-}
-
-   
 }
