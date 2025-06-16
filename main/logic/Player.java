fecf43ee2c492ebfef9fefc98600a737987d0878
JaeJae31 <jaeden200405@student-schoolsofwestfield.org>
2025-06-16 08:22:35 -0400

    Update Player.java

--- main/logic/Player.java
+++ main/logic/Player.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 public class Player {
     private String name;
+    private DiceCup cup;
     private Scoresheet playerScoresheet;
     Scanner input = new Scanner(System.in);
 
     // More methods may be necessary for accessing and mutating the Scoresheet object for each player.
 
-    public Player(String name, Scoresheet playerScoresheet) {
-        System.out.println("Enter Player Name: ");
-        this.name = input.nextLine();
-        this.playerScoresheet = playerScoresheet;
+    public Player(String name) {
+        // System.out.println("Enter Player Name: ");
+        this.name = name;
+        this.playerScoresheet = new Scoresheet();
+        this.cup = new DiceCup();
 
         input.close();
     }
