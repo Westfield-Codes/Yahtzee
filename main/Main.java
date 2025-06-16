025b2389016b7cc7ecba797dd0fd8753d6a0935b
JaeJae31 <jaeden200405@student-schoolsofwestfield.org>
2025-06-16 08:22:09 -0400

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -1,9 +1,21 @@
 package main;
 
-import main.functions.GameGraphics;
+import main.logic.GameGraphics;
+import main.logic.Player;
+import main.logic.DiceCup;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         GameGraphics.show();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        Player P1 = new Player(name);
+        
+        
+
     }
 }
