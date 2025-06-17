dd7cca531980860a10e8bf0f349368b90deeb2d9
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2025-06-17 08:38:25 -0400

    Update Main.java
    Round is not correctly implemented

--- main/Main.java
+++ main/Main.java
@@ -2,24 +2,30 @@ package main;
 
 import main.logic.GameGraphics;
 import main.logic.Player;
-import main.logic.DiceCup;
+import main.logic.Round;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.lang.Integer;
 
 public class Main {
     public static void main(String[] args) {
         GameGraphics.show();
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         ArrayList<Player> players = new ArrayList<>();
         System.out.print("How many players?: ");
-        int playersNum = scanner.nextLine();
-        for (int i = 0; i < playersNum; i++) {
+        String playersNum = sc.nextLine();
+        int playersCount = Integer.parseInt(playersNum);
+        if (playersNum == null) {
+            System.out.println("Invalid number of players. Please enter a valid integer.");
+            return;
+        }
+        for (int i = 0; i < playersCount; i++) {
             System.out.print("Enter your name: ");
-            String name = scanner.nextLine();
+            String name = sc.nextLine();
             Player player = new Player(name);
             players.add(player);
         }
-        Round game = new Round(players);
+        // Round game = new Round(players);
         // GameGraphics.showPlayers(players);
     }
 
